3c99d2b798c73c438349cd27b95203687376c505
GaelAlas <00033625@uca.edu.sv>
2025-05-12 21:11:03 -0600

    GaelAlas

---
+++ practica.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int option;
+    float salary;
+
+    cout<<"Input your salary to calculate your secure"<<endl;
+    cin>>salary;
+    if(salary<400.00){
+        cout<<"Sorry, but you cant request a life secure"<<endl;
+    }
+    else{
+        //Calculating the range
+        
+        
+    }
+
+
+    return 0;
+}
